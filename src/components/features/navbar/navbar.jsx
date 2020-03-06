e745c5c94a03b29d7d32f527df5ad868de1cbdc6
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-06 15:57:19 +0100

    implement navbar

---
+++ src/components/features/navbar/navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavbarText
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+
+import "./navbar.scss";
+
+const AppNavbar = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="nav-section">
+      <div className="stick-top">
+        <Navbar  light expand="md">
+          <NavbarBrand href="/" className="navbar-brand">
+            <span>React</span> Bank
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar className="mt-4">
+            <Nav className="mr-auto ml-auto" navbar>
+              <NavItem>
+                <NavLink to="/exchange">Exchage Rate</NavLink>
+              </NavItem>
+            </Nav>
+            <NavbarText className="mr-3">
+              <NavLink to="/login">
+                <i class="fas fa-sign-in-alt"></i> Login
+              </NavLink>
+            </NavbarText>
+            <NavbarText>
+              <NavLink to="/register">
+                {" "}
+                <i class="fa fa-user-plus"></i> Sign up 
+              </NavLink>
+            </NavbarText>
+          </Collapse>
+        </Navbar>
+      </div>
+    </div>
+  );
+};
+
+export default AppNavbar;
