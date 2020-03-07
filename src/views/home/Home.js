14ab7870bd71c0df3a3b61bd1349f6303120795f
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-03-07 08:39:30 +0100

    created  home view

---
+++ src/views/home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./home.scss"
+// import Carousel from "../../components/commons/Carousel";
+import Navbar from "../../components/features/navbar/navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      < div className="top-home"></div>
+
+      {/* <WhatWeDo/> */}
+    </div>
+  );
+}
